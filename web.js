ab0af57e4735879f18705f3ef239aee5a60ae225
Duane Wittaker <dwittaker@gmail.com>
2013-07-08 05:06:10 +0000

    Added index.html

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+var bfr = fs.readFileSync('index.html');
+response.send(bfr.toString());
+// response.send('Hello World2!');
 });
 
 var port = process.env.PORT || 5000;
